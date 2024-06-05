7c0ea0e2e492c9658a6fddff55282fc85f72cf52
SlowpokerFace <112822397+SlowpokerFace@users.noreply.github.com>
2024-06-06 02:34:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
+import random
+
+
+def destined_tea():
+    """it will choose your tea"""
+    tea = random.choice(['black', 'green'])
+    return tea
+
+
 def main():
     print("Slowpoker")
+    print(destined_tea())
+
 
 if __name__ == "__main__":
     main()
